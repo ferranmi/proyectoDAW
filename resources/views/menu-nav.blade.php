6ef083c9fdcfa952e50898195b73d880457db0e0
ferranmi <ferranmartinezivern2000@gmail.com>
2021-05-10 17:12:12 +0200

    includes

---
+++ resources/views/menu-nav.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.base')
+@section('content')
+    <nav>
+        <a href="#">Inicio</a>
+        <a href="#">Carreras</a>
+        <a href="#">Iniciar Sesion/Registrarse</a>
+        <a href="#">Preguntame algo</a>
+        <a href="#">Contacto</a>
+    </nav>
+@endsection
