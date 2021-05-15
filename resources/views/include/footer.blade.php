a03241055a61e36043abdaef4b6022aa67b088b2
ferranmi <ferranmartinezivern2000@gmail.com>
2021-05-15 12:08:16 +0200

    bbdd

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,1 +1,6 @@
-<a href="#">Contacte</a>
+<iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2995.1368376191117!2d2.020867815288395!3d41.349379379267596!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a49b79bbb7b6a5%3A0x58d4d335637bcc86!2sInstitut%20P%C3%BAblic%20Marianao!5e0!3m2!1sca!2ses!4v1620840914797!5m2!1sca!2ses"
+    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe><br>
+Telefon:999999999<br>
+Email: admin@trialrunningworld.es<br>
+Adreça: Passeig de les Mimoses, 18, 08830 Sant Boi de Llobregat, Barcelona
