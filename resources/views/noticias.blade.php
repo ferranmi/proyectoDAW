b96be2b4a05556d9efc92abbcd938cb37fee0427
ferranmi <79153164+ferranmi@users.noreply.github.com>
2021-05-20 16:11:24 +0200

    test

--- resources/views/noticias.blade.php
+++ resources/views/noticias.blade.php
@@ -1,14 +1,17 @@
-@extends('layouts.base'); @section('products') @foreach ($news as $new)
-    <div class="row featurette">
-        <div class="col-md-7">
-            <h2 class="featurette-heading"> <a href="noticias/{{ $new->code }}">
-                    {{ $new->title }}
+@extends('layouts.base');
+@section('products')
+    @foreach ($news as $new)
+        <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading"> <a href="noticias/{{ $new->code }}">
+                        titulo: {{ $new->title }}
 
-                </a></h2>
-            <span class="text-muted"> {{ $new->content }} </span>
-            <p class="lead"> {{ $new->commentaries }} </p>
-            <div class="col-md-5"> {{ $new->image }} </div>
+                    </a></h2>
+                <span class="text-muted"> contenido: {{ $new->content }} </span>
+                <p class="lead"> comentarios: {{ $new->commentaries }} </p>
+                <div class="col-md-5"> imatge: {{ $new->image }} </div>
 
-@endforeach
+    @endforeach
 
-{{ $news->links() }}
+    {{ $news->links() }}
+@endsection
